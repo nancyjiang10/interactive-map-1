724efbfaa50131d6ad6a723f0a70007c4d10560d
Nancy Jiang <nancy.jiang10@journalism.cuny.edu>
2026-04-27 11:32:59 -0400

    Add files via upload

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,10 +1,8 @@
-// Page settings
-// These values are passed to the layout to control what appears on the page.
+import litterBaskets from '$lib/data/litterBaskets.json';
 export function load() {
   return {
-    // Set to false to hide the NYCity News Service header
     showHeader: true,
-    // Set to false to hide the site footer
     showFooter: true,
+    litterBaskets,
   };
 }
